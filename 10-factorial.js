bc81a27c297be9f30e481d31303be57df5bac5d4
Nsix6 <nsikakmbre@gmail.com>
2025-09-22 05:00:52 +0100

    factorial

---
+++ 10-factorial.js
@@ -0,0 +1,14 @@
+const { argv } = require("node:process");
+
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = parseInt(argv[2]);
+console.log(factorial(num));
